d34288e8e0d80c59b0786576953b6defb783e18c
rustagimohit4-code <rustagimohit4@gmail.com>
2025-10-26 20:10:44 +0530

    Update Project.sol
    added startdate

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -4,10 +4,12 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Identity {
     string Project;
     string Description;
+    int startdate;
 
     constructor() {
         Project = "Stablecoin-on-Core";
         Description = unicode"Stablecoin on Core is a decentralized digital currency built on the Core Blockchain, designed to maintain a stable value pegged to fiat. It uses smart contracts, collateral reserves, and oracles to ensure transparency, stability, and secure on-chain transactions.";
+        startdate = 23102006;
     }
     
     function showProject() public view returns (string memory) {
@@ -16,5 +18,8 @@ contract Identity {
     
     function showDescription() public view returns (string memory) {
         return Description; 
+    }
+    function showstartDate() public view returns (int memory) {
+        return startdate; 
     }
 }
